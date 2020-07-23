7fbb34f3cbcc0f058944274a6b97df3184cd53c7
romanenkor <romu4444@gmail.com>
2020-07-23 19:39:01 +0300

    Modified the update script

--- update.sh
+++ update.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 cd $(dirname $0)
-
-GIT_DIR=$PWD/.git
+git config advice.addEmbeddedRepo false
 git add update.sh
 
-GIT_WORK_TREE=$HOME
+export GIT_DIR=$PWD/.git
+export GIT_WORK_TREE=$HOME
+
 [ -d "$HOME/.config/nvim" ] && git add $HOME/.config/nvim
 [ -d "$HOME/.config/tmux" ] && git add $HOME/.config/tmux
 [ -d "$HOME/.config/tmux" ] && git add $HOME/.tmux.conf
 [ -f "$HOME/.zshrc" ] && git add $HOME/.zshrc
 
-git status
+git commit
 git push -u origin master
